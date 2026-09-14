e39193a7f9a29c47611498401999ecd72bbd80a6
sudhanshu rai <sudhanshurai2005@gmail.com>
2026-09-14 17:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (63.72%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>&g, vector<int>&c) {
-        int n=g.size();
-       
-        vector<int>ps(2*n,0);
-        ps[0]=g[0]-c[0];
-        for(int i=1;i<2*n;i++)ps[i]=ps[i-1]-c[i%n]+g[i%n];
-        set<pair<int,int>>st;
-        for(int i=0;i<n-1;i++)st.insert({ps[i],i});
-        for(int i=n-1;i<2*n;i++){
-  st.insert({ps[i], i});
-            
-            int start = i - n + 1;
-            
-            int baseline = (start > 0) ? ps[start - 1] : 0;
-            
-          
-            if(!st.empty() && st.begin()->first >= baseline) {
-                return start;
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            int diff = gas[i] - cost[i];
+
+            total += diff;
+            tank += diff;
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
             }
-            
-           
-            st.erase({ps[start], start});
         }
-        return -1;
+
+        return total < 0 ? -1 : start;
     }
 };
