41c0784bbf2fbf862e5fb428294464d44e846b04
sudhanshu rai <sudhanshurai2005@gmail.com>
2026-09-18 10:25:13 +0530

    Time: 187 ms (5.29%) | Memory: 105.5 MB (5.06%) - LeetSync

---
+++ 456-132-pattern/132-pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>&v) {
+        vector<int>v1=v,v2=v;
+        int n=v.size();
+        int miv=INT_MAX;
+        for(int i=0;i<n;i++){
+            v1[i]=miv;
+            miv=min(miv,v[i]);
+        }
+        set<int>st;
+        st.insert(v[n-1]);
+        for(int i=n-1;i>=0;i--){
+            auto it=st.upper_bound(v1[i]);
+            if(it!=st.end()&&*it<v[i])return 1;
+            st.insert(v[i]);
+        }
+        return 0;
+    }
+};
