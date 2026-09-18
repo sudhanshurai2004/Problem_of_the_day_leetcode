4e815c16835807a40c838b003305a467aa01acfd
sudhanshu rai <sudhanshurai2005@gmail.com>
2026-09-18 14:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (13.20%) - LeetSync

---
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int median = nums[n / 2];
+        int moves = 0;
+        
+        for (int i = 0; i < n; i++) {
+            moves += abs(nums[i] - median);
+        }
+        
+        return moves;
+    }
+};
