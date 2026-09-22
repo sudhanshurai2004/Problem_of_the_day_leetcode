ce34507696405f89b3ec33815d07f972f32f3a56
sudhanshu rai <sudhanshurai2005@gmail.com>
2026-09-22 11:20:58 +0530

    Time: 2 ms (24.68%) | Memory: 8.5 MB (18.71%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,52 +1,24 @@
 #include <string>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 class Solution {
-private:
-    bool solve(int idx, string& pattern, vector<bool>& used, string& current, string& result) {
-        if (idx == pattern.length() + 1) {
-            result = current;
-            return true;
-        }
-
-        for (int digit = 1; digit <= 9; digit++) {
-            if (!used[digit]) {
-                if (idx == 0) {
-                    used[digit] = true;
-                    current.push_back(digit + '0');
-                    
-                    if (solve(idx + 1, pattern, used, current, result)) return true;
-                    
-                    current.pop_back();
-                    used[digit] = false;
-                } else {
-                    int prevDigit = current.back() - '0';
-                    char cond = pattern[idx - 1];
-
-                    if ((cond == 'I' && digit > prevDigit) || (cond == 'D' && digit < prevDigit)) {
-                        used[digit] = true;
-                        current.push_back(digit + '0');
-
-                        if (solve(idx + 1, pattern, used, current, result)) return true;
-
-                        current.pop_back();
-                        used[digit] = false;
-                    }
+public:
+    string smallestNumber(string pattern) {
+        string result = "";
+        stack<int> st;
+        
+        for (int i = 0; i <= pattern.length(); i++) {
+            st.push(i + 1);
+            
+            if (i == pattern.length() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result += to_string(st.top());
+                    st.pop();
                 }
             }
         }
-        return false;
-    }
-
-public:
-    string smallestNumber(string pattern) {
-        vector<bool> used(10, false);
-        string current = "";
-        string result = "";
-        
-        solve(0, pattern, used, current, result);
         
         return result;
     }
